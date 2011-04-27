9e58a1735ba77b18296657bb9d1c1c5a122855e7
stbartels <stbartels12@gmail.com>
2011-04-27 17:59:21 -0500

    Added helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Weblog"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +15,7 @@ describe PagesController do
     
     it 'should have the right title' do
       get 'home'
-      response.should have_selector('title', :content => 'Weblog | Home')
+      response.should have_selector('title', :content => '#{@base_title} | Home')
     end
     
     it 'should have a non-blank body' do
@@ -28,7 +32,7 @@ describe PagesController do
     
      it 'should have the right title' do
       get 'contact'
-      response.should have_selector('title', :content => 'Weblog | Contact')
+      response.should have_selector('title', :content => '#{@base_title} | Contact')
     end
   end
   
@@ -40,7 +44,7 @@ describe PagesController do
     
      it 'should have the right title' do
       get 'about'
-      response.should have_selector('title', :content => 'Weblog | About')
+      response.should have_selector('title', :content => '#{@base_title} | About')
     end
   end
 
