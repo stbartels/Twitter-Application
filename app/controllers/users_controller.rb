1760784acfaf3b408999906e6d897b65dcf8f00e
stbartels <stbartels12@gmail.com>
2011-05-03 07:38:32 -0500

    Editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,25 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      redirect_to @user, :flash => {:success => "Welcome to the Twitter knock off Weblog"}
+      redirect_to @user, :flash => {:success => "Welcome to the Twitter knock off Weblog!"}
     else
       @title = 'Sign up'
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => {:success => "Profile was successfully updated."}
+    else
+     @title = "Edit user"
+      render 'edit'
     end
   end
 end
